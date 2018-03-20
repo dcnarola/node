d4491ff9e45d9ffa243bfc7a61c52be53643c23e
dcnarola <37541128+dcnarola@users.noreply.github.com>
2018-03-20 22:06:29 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+var http = require("http");
+var fs=require("fs");
+var path=require("path");
+
+http.createServer(function(req,res){
+	
+	if(req.url==="/"){
+		fs.readFile('./public/index.html',"UTF-8",function(err,html){
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.end(html);
+});		
+	}else if(req.url.match("\.css$")){
+		var cssPath=path.join(__dirname,'public',req.url);
+		var fileStream=fs.createReadStream(cssPath,'UTF-8');
+		res.writeHead(200,{"Content-Type":"text/css"});
+		//console.log(cssPath);
+		fileStream.pipe(res);
+	}else if(req.url.match("\.png$")){
+		var imagePath=path.join(__dirname,'public',req.url);
+		var fileStream=fs.createReadStream(imagePath);
+		res.writeHead(200,{"Content-Type":"image/png"});
+		fileStream.pipe(res);
+	}else{
+		res.writeHead(404,{"Content-Type":"text/html"});
+		res.end("No page found");
+	}
+
+}).listen(3000);
+
+//console.log(req.url);
