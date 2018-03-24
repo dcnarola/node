267b38d8280f304ed650723f09625020e5565855
Darshansinh Chavda <cdarshansinh@narola.local>
2018-03-24 12:29:09 +0530

    check

--- insert.js
+++ insert.js
@@ -4,9 +4,9 @@ var fs=require("fs");
 var path=require("path");
 var querystring=require("querystring");
 var MongoClient=require('mongodb').MongoClient;
-var url='';
+var url="mongodb://localhost:27017/";
 
-http.createServer(function(req,res){
+/*http.createServer(function(req,res){
 	if(req.url==='/form'){
 		res.writeHead(200,{"Content-Type":"text/html"});
 		fs.createReadStream('./public/form.html','UTF-8').pipe(res);
@@ -22,4 +22,30 @@ http.createServer(function(req,res){
 			console.log(q);
 		}); 
 	}
-}).listen(3000); 
+}).listen(3000);*/ 
+/*create database and collections*/
+/*MongoClient.connect(url,function(err,db){
+if(err) throw err;
+var dbo=db.db("mydb");
+dbo.createCollection('customers',function(err,res){
+	if(err) throw err;
+	console.log("Collection created!!");
+	db.close();
+});
+});*/
+
+MongoClient.connect(url,function(err,db){
+if(err) throw err;
+var dbo=db.db("mydb");
+var myobj={name:"temp2",address:"mongolearn"};
+/*dbo.collection("customers").insertOne(myobj,function(err,res){
+	if(err) throw err;
+	console.log("one record inserted!!");
+	//db.close();
+});*/
+dbo.collection("customers").findOne({},function(err,result){
+	if(err) throw err;
+	console.log(result.name+'--'+result.address);
+	db.close();
+});
+});
